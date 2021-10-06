f5376187b6c814dce8936ed1a59d8693f7ff703e
michalbabos <michalbabos@gmail.com>
2021-10-06 17:52:42 +0200

    testgit origin

--- RestaurantApi/Entities/Dish.cs
+++ RestaurantApi/Entities/Dish.cs
@@ -1,7 +1,7 @@
 namespace RestaurantApi.Entities
 {
     public class Dish
-    {
+    {//testgit
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
